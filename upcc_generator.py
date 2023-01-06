ae32d421d604d80893ff2c714cd7bb775b44bb96
dgudtsov <denis@denis>
2023-01-06 20:23:21 +0300

    refactored

--- upcc_generator.py
+++ upcc_generator.py
@@ -4,6 +4,9 @@ Created on 6 янв. 2023 г.
 @author: denis
 '''
 
+#TODO
+# - files sharding
+
 import random
 
 IMSI_start = 401771000000001
@@ -13,7 +16,7 @@ subscribers_count = 100
 
 output_dir="./generator"
 
-profile = {
+profile_template = {
   "SID": "288755DBEFD7A8FA",
   "PCRFINDEX": "0",
   "SUBSCRIBERSN": "2807687AEFD7B0DE",
@@ -152,35 +155,40 @@ tag_end = "<SUBEND\n"
 
 #=== Code
 
+def create_subs(IMSI,MSISDN):
+
+    print ("creating "+str(MSISDN))
+
+    with open(output_dir+"/"+str(MSISDN)+".txt",'w') as f_out:
+    
+        f_out.write(tag_begin)
+        
+        # writing profile fields
+        for k in profile_template:
+            
+            if k=="MSISDN":
+                value=str(MSISDN)
+            elif k=="SUBSCRIBERIDENTIFIER":
+                value=str(IMSI)
+            else:
+                value = profile_template[k]    
+            
+            # fill template and write it
+            f_out.write (field_template.format(key = k, value = value))
+        
+        # writing QUOTA and SUBSCRIPTION fields
+        [ f_out.write(multi_fields[random.randrange(1,len(multi_fields)-1)]+"\n") for k in range(0,random.randrange(1,len(multi_fields))) ]
+              
+        f_out.write (tag_end)    
+    
+    return
+
 if __name__ == '__main__':
     
-    for i in range(0,subscribers_count):
-        
-        with open(output_dir+"/"+str(MSISDN_start+i)+".txt",'w') as f_out:
-        
-            f_out.write(tag_begin)
-            
-            for k in profile:
-                
-                value = profile[k]
-                if k=="MSISDN":
-                    value=str(MSISDN_start+i)
-                    
-                if k=="SUBSCRIBERIDENTIFIER":
-                    value=str(IMSI_start+i)
-                
-                kvp = field_template.format(key = k, value = value)
-                
-                f_out.write (kvp)
-            
-            [ f_out.write(multi_fields[random.randrange(1,len(multi_fields)-1)]+"\n") for k in range(0,random.randrange(1,len(multi_fields))) ]
-            
-#            for k in range(0,random.randrange(1,len(multi_fields))):
-#                f_out.write(multi_fields[random.randrange(1,len(multi_fields)-1)])
-#                f_out.write("\n")
-            
-            print ("creating "+str(MSISDN_start+i)+", number of multi fields: "+str(k))
-            
-            f_out.write (tag_end)
+    print("Generating: "+str(subscribers_count)+" subscribers")
     
-    pass
+    print("Output to: "+output_dir)
+    
+    list(map(create_subs,range(IMSI_start,IMSI_start+subscribers_count),range(MSISDN_start,MSISDN_start+subscribers_count)))
+    
+    exit(0)
